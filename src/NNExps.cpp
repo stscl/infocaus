db9306c66f88423c77eb3afcdddfb8bec08dc51d
SpatLyu <lyu.geosocial@gmail.com>
2026-03-19 00:00:01 +0800

    export neighbourhood utility

--- src/NNExps.cpp
+++ src/NNExps.cpp
@@ -47,21 +47,15 @@ Rcpp::List RcppNN4MatSub(
     bool byrow = true
 ) {
     // Convert Rcpp::NumericMatrix to std::vector<std::vector<double>>
-    int numRows = mat.nrow();
-    int numCols = mat.ncol();
-    std::vector<std::vector<double>> cppMat(numRows, std::vector<double>(numCols));
+    std::vector<std::vector<double>> cppMat = mat_r2std(mat, true);
 
-    for (int r = 0; r < numRows; ++r) {
-        for (int c = 0; c < numCols; ++c) {
-            cppMat[r][c] = mat(r, c);
-        }
-    }
+    const int n_obs = byrow ? mat.nrow() : mat.ncol(); 
 
     // Convert and check that lib and pred indices are within bounds & convert R based 1 index to C++ based 0 index
     std::vector<size_t> lib_std;
     lib_std.reserve(lib.size());
     for (int i = 0; i < lib.size(); ++i) {
-        if (lib[i] < 1 || lib[i] > numRows) {
+        if (lib[i] < 1 || lib[i] > n_obs) {
             Rcpp::stop("lib contains out-of-bounds index at position %d (value: %d)", i + 1, lib[i]);
         }
         lib_std.push_back(static_cast<size_t>(lib[i] - 1));
@@ -70,7 +64,7 @@ Rcpp::List RcppNN4MatSub(
     std::vector<size_t> pred_std;
     pred_std.reserve(pred.size());
     for (int i = 0; i < pred.size(); ++i) {
-        if (pred[i] < 1 || pred[i] > numRows) {
+        if (pred[i] < 1 || pred[i] > n_obs) {
             Rcpp::stop("pred contains out-of-bounds index at position %d (value: %d)", i + 1, pred[i]);
         }
         pred_std.push_back(static_cast<size_t>(pred[i] - 1));
@@ -78,7 +72,9 @@ Rcpp::List RcppNN4MatSub(
 
     // Call the neighbpurbood function
     std::vector<std::vector<size_t>> neighbours = NN::NN4Mat(
-        cppMat, lib_std, pred_std, static_cast<size_t>(std::abs(k)), method, include_self);
+        cppMat, lib_std, pred_std, 
+        static_cast<size_t>(std::abs(k)), 
+        method, include_self, byrow);
 
     // Return nb object (List in R side)
     return std2nb(neighbours);
