be20b3282978285546d0303844b7d610a4fc3194
SpatLyu <lyu.geosocial@gmail.com>
2026-03-20 00:00:01 +0800

    draft infotheo utility for continuous data

--- inst/include/ksginfo.hpp
+++ inst/include/ksginfo.hpp
@@ -214,7 +214,7 @@ inline double CE(
     std::vector<size_t> tc = cond;
     tc.insert(tc.end(),target.begin(),target.end());
 
-    return JE(mat,tc,k,alg,base) - JE(mat,cond,k,alg,base);
+    return return JE(mat,tc,k,base,alg) - JE(mat,cond,k,base,alg);
 }
 
 /***********************************************************
