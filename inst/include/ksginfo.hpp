b0c9dc3139ac44fc9d0e8901e95a25eec9232230
SpatLyu <lyu.geosocial@gmail.com>
2026-03-22 00:00:01 +0800

    remove epsilon clamping in ksginfo

--- inst/include/ksginfo.hpp
+++ inst/include/ksginfo.hpp
@@ -117,7 +117,8 @@ inline double Entropy(
             row.begin() + (k - 1),
             row.end());
 
-        double eps = std::max(row[k-1], 1e-12);
+        double eps = row[k-1];
+        // double eps = std::max(row[k-1], 1e-15);
 
         avg += std::log(eps);
     }
@@ -179,7 +180,8 @@ inline double JE(
             row.begin() + (k - 1),
             row.end());
 
-        double eps = std::max(row[k-1], 1e-12);
+        double eps = row[k-1];
+        // double eps = std::max(row[k-1], 1e-15);
 
         avg += std::log(eps);
     }
