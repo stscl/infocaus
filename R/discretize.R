fe409d17a5a760e97241e87279af6a325a9835d4
SpatLyu <lyu.geosocial@gmail.com>
2026-04-03 00:00:01 +0800

    enhance r interface

--- R/discretize.R
+++ R/discretize.R
@@ -32,7 +32,7 @@
 #' 
 discretize = \(x, n = 5, method = 'natural', large = 3000, prop = 0.15,
                seed = 123456789, thr = 0.4, iter = 100, bps = NULL, right_closed = TRUE){
-  if (any(inherits(x,'factor'),inherits(x,'character'))){
+  if (inherits(x,'factor') || inherits(x,'character')){
     return(as.integer(as.factor(x)))
   }
 
