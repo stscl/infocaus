6725559b97be51a9b1d22f7869837c5e4296f400
SpatLyu <lyu.geosocial@gmail.com>
2026-04-03 00:00:01 +0800

    update document

--- R/discretize.R
+++ R/discretize.R
@@ -28,6 +28,7 @@
 #'
 #' @examples
 #' infoxtr::discretize(1:100, n = 5, method = 'natural')
+#' 
 discretize = \(x, n = 5, method = 'natural', large = 3000, prop = 0.15,
                seed = 123456789, thr = 0.4, iter = 100, bps = NULL, right_closed = TRUE){
   if (any(inherits(x,'factor'),inherits(x,'character'))){
