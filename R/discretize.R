e2559a6f9a256deeeea2cd478d561237d084c0a9
SpatLyu <lyu.geosocial@gmail.com>
2026-04-03 00:00:01 +0800

    update document

--- R/discretize.R
+++ R/discretize.R
@@ -27,7 +27,7 @@
 #' @export
 #'
 #' @examples
-#' infocaus::discretize(1:100, n = 5, method = 'natural')
+#' infoxtr::discretize(1:100, n = 5, method = 'natural')
 #'
 discretize = \(x, n = 5, method = 'natural', large = 3000, prop = 0.15,
                seed = 123456789, thr = 0.4, iter = 100, bps = NULL, right_closed = TRUE){
