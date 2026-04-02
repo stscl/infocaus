d25d4636c8d458753b5912b8e03bace6f459b39b
SpatLyu <lyu.geosocial@gmail.com>
2026-04-03 00:00:01 +0800

    enhance r interface

--- R/entropy.R
+++ R/entropy.R
@@ -21,7 +21,7 @@
 #'
 entropy = \(vec, base = exp(1), type = c("cont", "disc"), k = 3) {
   type = match.arg(type)
-  vec = .check_vec1d(vec)
+  vec = .check_vec1d(vec, type)
   if (type == "disc") {
     return(RcppDiscEntropy(vec, base, TRUE))
   } else {
@@ -45,7 +45,7 @@ entropy = \(vec, base = exp(1), type = c("cont", "disc"), k = 3) {
 #'
 je = \(data, indices, base = exp(1), type = c("cont", "disc"), k = 3) {
   type = match.arg(type)
-  mat = .convert2mat(data)
+  mat = .convert2mat(data, type)
   if (type == "disc") {
     return(RcppDiscJE(mat, indices, base, TRUE))
   } else {
@@ -69,7 +69,7 @@ je = \(data, indices, base = exp(1), type = c("cont", "disc"), k = 3) {
 #'
 ce = \(data, target, conds, base = exp(1), type = c("cont", "disc"), k = 3) {
   type = match.arg(type)
-  mat = .convert2mat(data)
+  mat = .convert2mat(data, type)
   if (type == "disc") {
     return(RcppDiscCE(mat, target, conds, base, TRUE))
   } else {
@@ -93,7 +93,7 @@ ce = \(data, target, conds, base = exp(1), type = c("cont", "disc"), k = 3) {
 #'
 mi = \(data, target, interact, base = exp(1), type = c("cont", "disc"), k = 3, normalize = FALSE) {
   type = match.arg(type)
-  mat = .convert2mat(data)
+  mat = .convert2mat(data, type)
   if (type == "disc") {
     return(RcppDiscMI(mat, target, interact, base, TRUE, normalize))
   } else {
@@ -118,7 +118,7 @@ mi = \(data, target, interact, base = exp(1), type = c("cont", "disc"), k = 3, n
 #'
 cmi = \(data, target, interact, conds, base = exp(1), type = c("cont", "disc"), k = 3, normalize = FALSE) {
   type = match.arg(type)
-  mat = .convert2mat(data)
+  mat = .convert2mat(data, type)
   if (type == "disc") {
     return(RcppDiscCMI(mat, target, interact, conds, base, TRUE, normalize))
   } else {
