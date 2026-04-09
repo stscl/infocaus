7815e9c4810332af1b7a19d51aec5451f1942c1d
SpatLyu <lyu.geosocial@gmail.com>
2026-04-10 00:00:01 +0800

    draft kocmi utility

---
+++ inst/include/infoxtr/kocmi.hpp
@@ -0,0 +1,27 @@
+#ifndef INFOXTR_KOCMI_HPP
+#define INFOXTR_KOCMI_HPP
+
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <thread>
+#include <cstdint>
+#include <numeric>
+#include <algorithm>
+#include <stdexcept>
+#include "infoxtr/numericutils.hpp"
+#include "infoxtr/distance.hpp"
+#include "infoxtr/infotheo.hpp"
+#include <RcppThread.h>
+
+namespace infoxtr 
+{
+
+namespace kocmi 
+{
+
+} // namespace kocmi
+
+}
+
+#endif // INFOXTR_KOCMI_HPP
