6ecb9de227cd43ec9b29e48e3619b01132f51682
SpatLyu <lyu.geosocial@gmail.com>
2026-04-12 00:00:01 +0800

    implement kocmi r api

--- R/kocmi.R
+++ R/kocmi.R
@@ -21,6 +21,8 @@ kocmi = \(data, target, agent, conds, knockoff, null_knockoff = NULL,
           seed = 123456789, base = exp(1), method = "equal", contain_null = TRUE) {
   type = match.arg(type)
   mat = .convert2mat(data, type)
-  return(RcppKOCMI(mat, target, agent, conds, knockoff, null_knockoff, type, nboots,
-                   k, 0, threads, seed, base, method, contain_null))
+  knockoff = .convert2mat(knockoff, contain_type = FALSE)
+  null_knockoff = .convert2mat(null_knockoff, contain_type = FALSE)
+  return(RcppKOCMI(mat, target, agent, conds, knockoff, null_knockoff, type,
+                   nboots, k, 0, threads, seed, base, method, contain_null))
 }
