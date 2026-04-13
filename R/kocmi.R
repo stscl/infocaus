dd42a9037ed792ff6cc7574545b973f9be55e15b
SpatLyu <lyu.geosocial@gmail.com>
2026-04-14 00:00:01 +0800

    enhance kocmi utility

--- R/kocmi.R
+++ R/kocmi.R
@@ -44,7 +44,7 @@ kocmi = \(data, target, agent, conds, knockoff, null_knockoff = NULL,
           type = c("cont", "disc"), nboots = 1e4, k = 3, threads = 1,
           seed = 42, base = exp(1), method = "equal", contain_null = TRUE) {
   type = match.arg(type)
-  mat = .convert2mat(data, type)
+  mat = .convert2mat(data, contain_type = FALSE)
   knockoff = .convert2mat(knockoff, contain_type = FALSE)
   null_knockoff = .convert2mat(null_knockoff, contain_type = FALSE)
   return(RcppKOCMI(mat, target, agent, conds, knockoff, null_knockoff, type,
