491235e07922bbaa82b706356104597bcaf2047a
SpatLyu <lyu.geosocial@gmail.com>
2026-04-21 00:00:01 +0800

    enhance surd utility

--- R/surd.R
+++ R/surd.R
@@ -1,23 +1,23 @@
 .surd_ts = \(data, target, agent, lag = 1, bin = 5, method = "equal",
              max.order = 10, threads = 1, base = 2.0, normalize = TRUE) {
   mat = .convert2mat(data, contain_type = FALSE)
-  return(RcppSURD(mat, target, agent, lag, bin, max.order, 
-                  threads, base, normalize, method))
+  return(RcppSURD(mat, abs(target), abs(agent), lag, max.order, 
+                  threads, base, normalize, abs(bin), method))
 }
 
 .surd_lattice = \(data, target, agent, lag = 1, bin = 5, method = "equal", 
                   max.order = 10, threads = 1, base = 2.0, normalize = TRUE, nb = NULL) {
   if (is.null(nb)) nb = sdsfun::spdep_nb(data)
   mat = .convert2mat(data, contain_type = FALSE)
-  return(RcppSURD(mat, target, agent, lag, bin, max.order, 
-                  threads, base, normalize, method, nb))
+  return(RcppSURD(mat, abs(target), abs(agent), lag, max.order, 
+                  threads, base, normalize, abs(bin), method, nb))
 }
 
 .surd_grid = \(data, target, agent, lag = 1, bin = 5, method = "equal",
                max.order = 10, threads = 1, base = 2.0, normalize = TRUE) {
   mat = .convert2mat(data, contain_type = FALSE)
-  return(RcppSURD(mat, target, agent, lag, bin, max.order, 
-                  threads, base, normalize, method, NULL, terra::nrow(data[[1]])))
+  return(RcppSURD(mat, abs(target), abs(agent), lag, max.order, threads, base, 
+                  normalize, abs(bin), method, NULL, terra::nrow(data[[1]])))
 }
 
 #' SURD
